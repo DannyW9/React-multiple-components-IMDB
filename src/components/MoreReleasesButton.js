cbf069b5d102cf965137250d8276e04e200f2ea7
Danny Welsh <dannywelsh400@hotmail.com>
2019-06-18 09:30:50 +0100

    Changes MoreReleasesButton to open in new tab

--- src/components/MoreReleasesButton.js
+++ src/components/MoreReleasesButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const MoreReleasesButton = () => {
   return(
-    <form action="https://www.imdb.com/calendar/?region=gb">
+    <form action="https://www.imdb.com/calendar/?region=gb" target="_blank">
       <input type="submit" value="See more upcoming releases" />
     </form>
   );
